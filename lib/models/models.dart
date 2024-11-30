08d46a772530a71763f40fc89b2fc6adf140d124
raulsg0108 <146940314+raulsg0108@users.noreply.github.com>
2024-11-30 20:49:19 +0100

    Update models.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,4 +1,4 @@
 enum Language { english, spanish }
-enum ValidationResult { win, continue_, invalid, notInWordList }
+enum ValidationResult { win, continue_, invalid, notInWordList, end }
 enum TileState { empty, wrong, wrongPosition, correct }
 enum KeyState { unused, wrong, wrongPosition, correct }
